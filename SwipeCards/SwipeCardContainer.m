7420d8d6d3fd390f0e25feef1dc3109e3535b79e
Stuart Farmer <email@stuartfarmer.me>
2015-11-17 19:11:45 -0500

    animations and interaction of collection completed

--- SwipeCards/SwipeCardContainer.m
+++ SwipeCards/SwipeCardContainer.m
@@ -8,45 +8,90 @@
 
 #import "SwipeCardContainer.h"
 
-#define BUTTONWIDTH 80
-#define BUTTONHEIGHT 20
+#define VIEWWIDTH 80
+#define VIEWHEIGHT 20
+
+#define DISTANCETHRESHOLD 50
+#define ANGLETHRESHOLD 50
 
 @implementation SwipeCardContainer {
-    UIButton *button;
+    UIView *swipeView;
     CGPoint startingPoint;
+    CGPoint firstPoint;
+    CGPoint currentPoint;
+    CGPoint upVector;
     float totalDistance;
 }
 
 -(void)didMoveToWindow {
+    // Set upVector for comparing angles against
+    upVector.x = 0;
+    upVector.y = 1;
+    
     // Draw everything
-    button = [[UIButton alloc] initWithFrame:CGRectMake(self.center.x-(BUTTONWIDTH/2), self.center.y-(BUTTONHEIGHT/2), BUTTONWIDTH, BUTTONHEIGHT)];
-    button.titleLabel.text = @"Something";
-    button.backgroundColor = [UIColor grayColor];
-    [self addSubview:button];
+    swipeView = [[UIButton alloc] initWithFrame:CGRectMake(self.center.x-(VIEWWIDTH/2), self.center.y-(VIEWHEIGHT/2), VIEWWIDTH, VIEWHEIGHT)];
+    swipeView.backgroundColor = [UIColor grayColor];
+    [self addSubview:swipeView];
 }
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     // Get the starting touch
     UITouch *touch = [touches anyObject];
-    startingPoint = [touch locationInView:self];
+    startingPoint = swipeView.center;
+    firstPoint = [touch locationInView:self];
+    currentPoint = [touch locationInView:self];
 
-    // Restart the total distance
+    // Reset the total distance
     totalDistance = 0;
 }
 
 -(void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    // Move the button (or view in this case) to the amount dragged on the screen
+    // Move the view (or view in this case) to the amount dragged on the screen
     CGPoint newPoint = [[touches anyObject] locationInView:self];
-    CGPoint difference = CGPointSubtract(newPoint, startingPoint);
-    button.center = CGPointAdd(button.center, difference);
-    startingPoint = newPoint;
-    NSLog(@"%f", CGPointGetDistance(newPoint, startingPoint));
-    // Add distance to total distance
-    totalDistance += CGPointGetDistance(newPoint, startingPoint);
+    CGPoint difference = CGPointSubtract(newPoint, currentPoint);
+    swipeView.center = CGPointAdd(swipeView.center, difference);
+    
+    // Color UI elements if view is within valid zone
+    if ([self viewIsWithinValidZone:swipeView]) {
+        swipeView.backgroundColor = [UIColor greenColor];
+    } else {
+        swipeView.backgroundColor = [UIColor grayColor];
+    }
+    
+    // Set current point to new point
+    currentPoint = newPoint;
 }
 
 -(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    NSLog(@"%f", totalDistance);
+    // Check if the view is in valid zone
+    if ([self viewIsWithinValidZone:swipeView]) {
+        NSLog(@"It's good!");
+    } else {
+        NSLog(@"It's bad...");
+    }
+    
+    // Animate view away
+    [UIView animateWithDuration:0.3 animations:^{
+        CGPoint difference = CGPointSubtract(swipeView.center, startingPoint);
+        CGPoint multiple = CGPointMultiply(difference, 10);
+        swipeView.center = CGPointAdd(swipeView.center, multiple);
+        swipeView.alpha = 0;
+    } completion:^(BOOL finished) {
+         // Reset the view
+        swipeView.center = startingPoint;
+        swipeView.alpha = 1;
+        swipeView.backgroundColor = [UIColor grayColor];
+    }];
+}
+
+-(BOOL)viewIsWithinValidZone:(UIView *)view {
+    // Calculate the current angle
+    CGPoint distanceFromStartingPoint = CGPointSubtract(startingPoint, view.center);
+    CGFloat angle = CGPointGetAngleBetween(distanceFromStartingPoint, upVector) * (180 / M_PI);
+    
+    // Color view appropriately if it is within the 'good' zone
+    if (angle < ANGLETHRESHOLD && CGPointGetDistance(startingPoint, view.center) > DISTANCETHRESHOLD) return true;
+    else return false;
 }
 
 @end
